bb54dc65668af5eda80789984e8d904dd667c370
Gauravjain <gaurjain@gmail.com>
2019-02-23 10:28:06 -0700

    Initial Commit : Helloworld app.main program commited. Test passed. trying again...

--- src/main/java/com/patterns/programming/functional/App.java
+++ src/main/java/com/patterns/programming/functional/App.java
@@ -9,6 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Jai Jinendra ! Gaurav P Jain" );
-        System.out.println("Hello World !!!");
+        System.out.println("Hello World !!! Welcome");
     }
 }

c65d9b99f5f261fc752060f58557688e6d4c7297
Gauravjain <gaurjain@gmail.com>
2019-02-23 20:16:01 -0700

    minor formatting changes.

--- src/main/java/com/patterns/programming/functional/App.java
+++ src/main/java/com/patterns/programming/functional/App.java
@@ -10,14 +10,14 @@ public class App
     {
         Thread thread1 = new Thread(new Task());
         thread1.start();
-        System.out.println( "Jai Jinendra ! from Gaurav P Jain" );
-        System.out.println("Thread name :" + Thread.currentThread().getName());
+        System.out.println( "Jai Jinendra !!! from Gaurav P Jain" );
+        System.out.println("Thread name : " + Thread.currentThread().getName());
 
     }
 
     static class Task implements Runnable {
         public void run() {
-            System.out.println("Hello World Bhai, Thread name !!!:" + Thread.currentThread().getName());
+            System.out.println("Hello World bhai, Thread name : " + Thread.currentThread().getName());
         }
     }
 }

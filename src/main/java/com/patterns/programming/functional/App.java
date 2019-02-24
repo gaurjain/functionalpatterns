07463ba8b70ace50f5314b09fdc70cd2c19203e2
Gauravjain <gaurjain@gmail.com>
2019-02-23 20:09:26 -0700

    Initial commit: Program with java executorservice introduction.

--- src/main/java/com/patterns/programming/functional/App.java
+++ src/main/java/com/patterns/programming/functional/App.java
@@ -8,7 +8,16 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Jai Jinendra ! .Gaurav P Jain" );
-        System.out.println("Hello World Bhai !!!");
+        Thread thread1 = new Thread(new Task());
+        thread1.start();
+        System.out.println( "Jai Jinendra ! from Gaurav P Jain" );
+        System.out.println("Thread name :" + Thread.currentThread().getName());
+
+    }
+
+    static class Task implements Runnable {
+        public void run() {
+            System.out.println("Hello World Bhai, Thread name !!!:" + Thread.currentThread().getName());
+        }
     }
 }

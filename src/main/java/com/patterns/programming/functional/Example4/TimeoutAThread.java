5809b565a18d1a1077c6ca3f2a93dda10b40a9c0
Gauravjain <gaurjain@gmail.com>
2019-03-05 23:13:30 -0700

    added a new example covering how to timeout a thread with interrupts.

--- src/main/java/com/patterns/programming/functional/Example4/TimeoutAThread.java
+++ src/main/java/com/patterns/programming/functional/Example4/TimeoutAThread.java
@@ -2,7 +2,6 @@ package com.patterns.programming.functional.Example4;
 
 
 import java.util.List;
-import java.util.concurrent.Executor;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
@@ -17,23 +16,28 @@ import java.util.concurrent.TimeUnit;
 public class TimeoutAThread {
 
     public static void main(String[] args)
-    {   //step 1: instantiate a threadpool.
-        ExecutorService service = Executors.newCachedThreadPool();
-        //step 2: pass on a task to the service thread pool for execution.
+    {   //step 1: instantiate a thread-pool.
+        ExecutorService service = Executors.newFixedThreadPool(10);
+
+        //step 2: pass on runnable tasks to the service thread pool for execution.
+        for(int i =0 ; i<20; i++) {
             service.submit(() -> {
-                while (!Thread.currentThread().isInterrupted()) {
+                if (!Thread.currentThread().isInterrupted()) {
                     try {
-                        System.out.println("Hello World, Task bearer's name : " + Thread.currentThread().getName());
-                        TimeUnit.SECONDS.sleep(1);
+                        System.out.println("Hello World, Task bearer's name : " + Thread.currentThread().getName() + "..going to sleep");
+                        TimeUnit.SECONDS.sleep(60);
+                        System.out.println("I am "+ Thread.currentThread().getName() +" my sleep is over");
                     } catch (InterruptedException ex) {
                         throw new IllegalStateException(ex);
                     }
                 }
             });
+        }
 
         //step 3: print something
-        System.out.println("main thread is executing steps in parallel");
-        //step 4: timeout for 1 minute (unrelated operation
+        System.out.println("main thread is executing steps in parallel...going to sleep");
+
+        //step 4: timeout  main thread for 1 minute (unrelated operation)
         try {
             TimeUnit.MINUTES.sleep(1);
             System.out.println("main thread's sleep is over!!");
@@ -44,9 +48,13 @@ public class TimeoutAThread {
 
         //step 5: stop the thread by requesting thread-pool shutdown.
         List<Runnable> runnables = service.shutdownNow();
+        /*
+        This internally calls thread.interrupt
+        for all running threads.
+        */
+
+        //step 6: print not yet shutdown runnables tasks
         System.out.println((runnables));
-        //This internally calls thread.interrupt
-        // for all running threads.
     }
 
 }

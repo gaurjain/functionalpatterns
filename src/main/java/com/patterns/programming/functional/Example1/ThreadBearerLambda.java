f4f0eb64992c15a6bbb73698b9fc214d0c27d47e
Gaurav P Jain1 <Gaurav.P.Jain1@aexp.com>
2019-03-05 17:25:19 -0700

    lambda expression for the runnable task instantiation in format ()-->{//runnable task steps}

--- src/main/java/com/patterns/programming/functional/Example1/ThreadBearerLambda.java
+++ src/main/java/com/patterns/programming/functional/Example1/ThreadBearerLambda.java
@@ -11,10 +11,8 @@ public class ThreadBearerLambda
     public static void main( String[] args )
     {
         for (int i = 0; i<10; i++) {
-            //loop to create a new threads and
-            //assign a new instance of runnable task
-            //to the thread.
-            Thread thread = new Thread(()->{
+            // lambda expression for the runnable task instantiation in format ()-->{//runnable task steps}
+            Thread thread = new Thread(() -> {
                 //task step 1
                 System.out.println("Hello World,from the Thread named : " + Thread.currentThread().getName());
                 try {
@@ -26,10 +24,11 @@ public class ThreadBearerLambda
                 //task step 3
             });
 
+            System.out.println("about to start threads " + i);
             thread.start(); //start the created task/thread.
         }
 
-        System.out.println( "Jai Jinendra!!!from the task bearer Gaurav P Jain" );
+        System.out.println( "Jai Jinendra, from the task bearer Gaurav P Jain" );
         System.out.println("Task-bearer thread's name : " + Thread.currentThread().getName());
     }
 
